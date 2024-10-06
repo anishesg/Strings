7f2ed678f46b538cf7059598c7dfb9c69e9b1889
anishesg <ak8686@princeton.edu>
2024-10-05 20:36:08 -0400

    Added str.h with function declarations

--- replace.c
+++ replace.c
@@ -1,6 +1,6 @@
 /*--------------------------------------------------------------------*/
 /* replace.c                                                          */
-/* Author: ???                                                        */
+/* Author: Anish Kataria                                               */
 /*--------------------------------------------------------------------*/
 
 #include "str.h"
@@ -20,7 +20,52 @@
 static size_t replaceAndWrite(const char *pcLine,
                               const char *pcFrom, const char *pcTo)
 {
-   /* Insert your code here. */
+    /* validate that the input pointers are not NULL */
+    assert(pcLine != NULL);
+    assert(pcFrom != NULL);
+    assert(pcTo != NULL);
+    
+    const char *currentPtr = pcLine;
+    size_t replacementCount = 0;
+    
+    size_t fromLength = Str_getLength(pcFrom);
+    size_t toLength = Str_getLength(pcTo);
+    
+    /* handle empty pcFrom string */
+    if (fromLength == 0) {
+        printf("%s", pcLine);
+        return 0;
+    }
+    
+    while (*currentPtr != '\0') {
+        /* search for the next occurrence of pcFrom in pcLine */
+        char *found = Str_search(currentPtr, pcFrom);
+        
+        if (found != NULL) {
+            /* calculate the number of characters before the found substring */
+            size_t prefixLength = (size_t)(found - currentPtr);
+            
+            /* print the prefix part that does not include pcFrom */
+            if (prefixLength > 0) {
+                printf("%.*s", (int)prefixLength, currentPtr);
+            }
+            
+            /* print the replacement string pcTo */
+            printf("%s", pcTo);
+            
+            /* move the currentPtr pointer past the found pcFrom substring */
+            currentPtr += prefixLength + fromLength;
+            
+            /* increment the replacement count */
+            replacementCount++;
+        } else {
+            /* no more occurrences found; print the remaining part of pcLine */
+            printf("%s", currentPtr);
+            break;
+        }
+    }
+    
+    return replacementCount;
 }
 
 /*--------------------------------------------------------------------*/
@@ -38,26 +83,29 @@ static size_t replaceAndWrite(const char *pcLine,
 
 int main(int argc, char *argv[])
 {
-   enum {MAX_LINE_SIZE = 4096};
-   enum {PROPER_ARG_COUNT = 3};
+    enum {MAX_LINE_SIZE = 4096};
+    enum {PROPER_ARG_COUNT = 3};
 
-   char acLine[MAX_LINE_SIZE];
-   char *pcFrom;
-   char *pcTo;
-   size_t uReplaceCount = 0;
+    char acLine[MAX_LINE_SIZE];
+    char *pcFrom;
+    char *pcTo;
+    size_t uReplaceCount = 0;
 
-   if (argc != PROPER_ARG_COUNT)
-   {
-      fprintf(stderr, "usage: %s fromstring tostring\n", argv[0]);
-      return EXIT_FAILURE;
-   }
+    if (argc != PROPER_ARG_COUNT)
+    {
+        fprintf(stderr, "usage: %s fromstring tostring\n", argv[0]);
+        return EXIT_FAILURE;
+    }
 
-   pcFrom = argv[1];
-   pcTo = argv[2];
+    pcFrom = argv[1];
+    pcTo = argv[2];
 
-   while (fgets(acLine, MAX_LINE_SIZE, stdin) != NULL)
-      /* Insert your code here. */
+    while (fgets(acLine, MAX_LINE_SIZE, stdin) != NULL) {
+        uReplaceCount += replaceAndWrite(acLine, pcFrom, pcTo);
+    }
 
-   fprintf(stderr, "%lu replacements\n", (unsigned long)uReplaceCount);
-   return 0;
+    fprintf(stderr, "%zu replacements\n", uReplaceCount);
+    return 0;
 }
+
+/*--------------------------------------------------------------------*/
--- str.h
+++ str.h
@@ -1,6 +1,6 @@
 /* str.h
- * Header file for the Str module.
- * Author: anish kataria
+ * header file for the str module.
+ * author: anish kataria
  */
 
 #ifndef STR_H
@@ -8,28 +8,70 @@
 
 #include <stddef.h> /* for size_t */
 
-/* Function declarations */
+/* function declarations */
 
-/* Return the length of the string pcSrc. */
+/* 
+ * function: str_getlength
+ * -----------------------
+ * calculates the number of characters in the given string
+ *
+ * pcsrc: the string whose length is to be determined
+ *
+ * returns: the length of the string as a size_t value
+ */
 size_t Str_getLength(const char pcSrc[]);
 
-/* Copy the string pcSrc to pcDest. Return pcDest. */
+/* 
+ * function: str_copy
+ * -------------------
+ * copies the source string into the destination array
+ *
+ * pcdest: the array where the string will be copied
+ * pcsrc:  the string to be copied
+ *
+ * returns: a pointer to the destination string pcdest
+ */
 char *Str_copy(char pcDest[], const char pcSrc[]);
 
-/* Add pcSrc to the end of pcDest. Return pcDest. */
+/* 
+ * function: str_concat
+ * ---------------------
+ * appends the source string to the end of the destination string
+ *
+ * pcdest: the array where the source string will be concatenated
+ * pcsrc:  the string to append to the destination
+ *
+ * returns: a pointer to the concatenated destination string pcdest
+ */
 char *Str_concat(char pcDest[], const char pcSrc[]);
 
-/* Compare pcStr1 and pcStr2 lexicographically.
- * Return a negative value if pcStr1 < pcStr2,
- * zero if they are equal,
- * or a positive value if pcStr1 > pcStr2.
+/* 
+ * function: str_compare
+ * ----------------------
+ * compares two strings to determine their lexicographical order
+ *
+ * pcstr1: the first string to compare
+ * pcstr2: the second string to compare
+ *
+ * returns: 
+ *   -1 if pcStr1 is less than pcStr2,
+ *    0 if pcStr1 is equal to pcStr2,
+ *    1 if pcStr1 is greater than pcStr2
  */
 int Str_compare(const char pcStr1[], const char pcStr2[]);
 
-/* Search pcHaystack for the substring pcNeedle.
- * Return a pointer to the first occurrence of pcNeedle in pcHaystack,
- * or NULL if it's not found.
+/* 
+ * function: str_search
+ * ---------------------
+ * searches for the first occurrence of the needle string within the haystack string
+ *
+ * pchydstack: the string to be searched within
+ * pcneedle:   the string to search for
+ *
+ * returns:
+ *   a pointer to the first occurrence of pcNeedle in pcHaystack,
+ *   or NULL if the needle is not found
  */
 char *Str_search(const char pcHaystack[], const char pcNeedle[]);
 
-#endif /* STR_H */
+#endif /* STR_H */ss
